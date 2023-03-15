a065df90614af620a72f1c69822c09b95312850e
jisoo <skscjswo32@gmail.com>
2023-03-15 15:03:34 +0900

    11

---
+++ lecture-source/05_router/03_params/src/pages/MenuSearchResult.js
@@ -0,0 +1,19 @@
+import { useEffect } from 'react';
+
+function MenuSearchResult() {
+
+    useEffect(
+        () => {
+
+        },
+        []
+    );
+
+    return (
+        <div>
+            <h1>검색된 메뉴</h1>
+        </div>
+    );
+}
+
+export default MenuSearchResult;
