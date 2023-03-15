6c35fc632c00986a900c04905715dd59bd370d9a
jisoo <skscjswo32@gmail.com>
2023-03-15 15:29:56 +0900

    11

--- lecture-source/05_router/03_params/src/apis/MenuAPI.js
+++ lecture-source/05_router/03_params/src/apis/MenuAPI.js
@@ -10,3 +10,8 @@ export function getMenuDetail(menuCode) {
     
     return menus.filter(menu => menu.menuCode === parseInt(menuCode))[0];
 }
+
+export function searchMenu(menuName) {
+
+    return menus.filter(menu => menu.menuName.match());
+}
